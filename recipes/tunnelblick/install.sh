d70bf24ec4adb6c5a83cc9c6652a885285d7630c
Ian Warner <ian.warner@drykiss.com>
2017-09-25 18:00:08 +0300

    added Tunnelblick

---
+++ recipes/tunnelblick/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tunnelblick
+#
+# @author Ian Warner <ian.warner@drykiss.com>
+# @category recipe
+# @see https://tunnelblick.net/
+
+header "Install Tunnelblick VPN Client"
+
+log "Continue (y/Y)"
+read TUNNELBLICK_CONTINE
+
+if [[ $TUNNELBLICK_CONTINE == y || $TUNNELBLICK_CONTINE == Y ]] ; then
+  brew cask install tunnelblick
+fi
--- recipes/zsh/install.sh
+++ recipes/zsh/install.sh
@@ -9,6 +9,9 @@ read ZSH_CONTINE
 
 if [[ $ZSH_CONTINE == y || $ZSH_CONTINE == Y ]] ; then
 
+  # May need to create the .zsh file first so we can source this
+
+  # Install ZSH
   subheader "Brew Install ZSH"
   brew update && brew install zsh
 
@@ -16,7 +19,7 @@ if [[ $ZSH_CONTINE == y || $ZSH_CONTINE == Y ]] ; then
   subheader "Set default shell to ZSH"
   sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 
-  #
+  # Restart
   subheader "Really need to restart the terminal here"
 
 fi
